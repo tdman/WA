d8989cbdafc97b605a18b4912fff60ccac58ec67
sumi <1@1>
2025-06-28 16:03:57 +0900

    문제 이미지와 확인

--- backend/src/main/java/aws/community/examples/bedrock/util/S3Util.java
+++ backend/src/main/java/aws/community/examples/bedrock/util/S3Util.java
@@ -5,6 +5,7 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -24,7 +25,7 @@ public class S3Util {
     public JSONObject readS3File (String bucketName, String key) throws Exception {
         JSONObject ret = new JSONObject();
         try {
-            // 예: s3Client.getObject(GetObjectRequest.builder().bucket(bucketName).key(key).build());
+// 예: s3Client.getObject(GetObjectRequest.builder().bucket(bucketName).key(key).build());
             GetObjectRequest getObjectRequest = GetObjectRequest.builder()
                     .bucket(bucketName)
                     .key(key)
@@ -38,5 +39,23 @@ public class S3Util {
             throw new Exception("S3Util ERROR: " + e.getMessage());
         }
         return ret;
+    }
+
+    public byte[] readS3Img (String bucketName, String key) throws Exception {
+        try {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            // S3에서 이미지 바이트로 읽어오기
+            ResponseBytes<GetObjectResponse> objectBytes = s3Client.getObjectAsBytes(getObjectRequest);
+            byte[] imgBytes = objectBytes.asByteArray();
+            log.info("Image content from S3: {} bytes", imgBytes.length);
+            return imgBytes;
+        } catch (Exception e) {
+            log.error("Error reading image from S3:", e);
+            throw new Exception("S3Util ERROR: " + e.getMessage());
+        }
     }
 }
