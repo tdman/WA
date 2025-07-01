ad867fb5523de291889bc92373b68bde02860591
hjoh <moonsanqwer@gmail.com>
2025-07-01 22:11:39 +0900

    충돌해결

--- backend/src/main/java/aws/community/examples/bedrock/controller/AuthController.java
+++ backend/src/main/java/aws/community/examples/bedrock/controller/AuthController.java
@@ -4,21 +4,23 @@ import aws.community.examples.bedrock.common.CmResponse;
 import aws.community.examples.bedrock.common.CmResponseFactory;
 import aws.community.examples.bedrock.dto.StudentDto;
 import aws.community.examples.bedrock.mapper.StudentMapper;
+
 import aws.community.examples.bedrock.service.ProgressService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.context.annotation.Description;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @Tag(name = "인증 및 사용자 관리 API", description = "인증 및 사용자 관리 API")
-@Slf4j
+
 public class AuthController {
 
     private final StudentMapper studentMapper;
@@ -33,7 +35,7 @@ public class AuthController {
           
         } catch (Exception e) {
             log.error("AuthController signup : ", e);
-            return CmResponseFactory.fail("학생 등록 실패");
+            return CmResponseFactory.fail("학생등록 실패");
         }
 		
 	}
