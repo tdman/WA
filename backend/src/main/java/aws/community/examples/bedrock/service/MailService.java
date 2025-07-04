d4b85568ec6d08d558a51a11811bceeec19d7225
sumi <1@1>
2025-07-05 05:17:20 +0900

    메일 서비스 추가

---
+++ backend/src/main/java/aws/community/examples/bedrock/service/MailService.java
@@ -0,0 +1,36 @@
+package aws.community.examples.bedrock.service;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.activation.DataSource;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.util.ByteArrayDataSource;
+
+
+@Service
+public class MailService {
+    private final JavaMailSender mailSender;
+
+    public MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMailWithInlineImageBytes(String to, String subject, String htmlText, byte[] imageBytes, String contentType) throws MessagingException {
+        String html = "<html><body>"+ htmlText +"<br><img src='cid:image1'></body></html>";// Set the recipient, subject, and HTML content
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(html, true);
+
+        DataSource dataSource = new ByteArrayDataSource(imageBytes, contentType);
+        helper.addInline("image1", dataSource);
+
+        mailSender.send(message);
+    }
+}
