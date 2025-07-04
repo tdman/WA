49fc7d6b9869461c0a45de4ea0c7a98ed08b7630
sumi <1@1>
2025-07-05 04:53:46 +0900

    UI 수정 및 목업 화면 추가(merge fix)
    - 테스트 확인용 라우터 경로 복원

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-
-import PrivateRoute from '../context/PrivateRoute';
+import LoginPage from '../pages/LoginPage';
+import StudentSignup from '../pages/StudentSignup';
+import MainPage from '../pages/MainPage';
+import ProblemPage from '../pages/ProblemPage';
+import FeedbackPage from '../pages/FeedbackPage';
+import ChatPage from '../pages/ChatPage';
+import TutorPage from '../pages/TutorPage';
+import TutorDetailPage from '../pages/TutorDetailPage';
 import TtoroChat from "../components/TtoroChat.jsx";
 import TtoroChat2 from "../components/TtoroChat2.jsx";
+import PrivateRoute from '../context/PrivateRoute';
+import ProgressPage from "../pages/ProgressPage.jsx";
 
 /**
  * routes/AppRoutes.js - 화면 라우팅 정의
@@ -12,17 +20,15 @@ import TtoroChat2 from "../components/TtoroChat2.jsx";
 function AppRoutes() {
     return (
         <Routes>
-            {/*<Route path="/" element={<LoginPage/>}/>*/}
-            {/*<Route path="/signup" element={<StudentSignup/>}/>*/}
-            {/*<Route path="/main" element={<PrivateRoute><MainPage/></PrivateRoute>}/>*/}
-            {/*<Route path="/problems" element={<PrivateRoute><ProblemPage/></PrivateRoute>}/>*/}
-            {/*<Route path="/feedback" element={<PrivateRoute><FeedbackPage/></PrivateRoute>}/>*/}
-            {/*<Route path="/chat" element={<PrivateRoute><ChatPage/></PrivateRoute>}/>*/}
-            {/*<Route path="/tutor" element={<PrivateRoute><TutorPage/></PrivateRoute>}/>*/}
-            {/*<Route path="/tutorDetail/:tutorId" element={<PrivateRoute><TutorDetailPage/></PrivateRoute>}/>*/}
-            {/*<Route path="/progress" element={<PrivateRoute><ProgressPage/></PrivateRoute>}/>*/}
-            {/*<Route path="/main" element={<PrivateRoute><TtoroChat/></PrivateRoute>}/>*/}
+            <Route path="/" element={<LoginPage/>}/>
+            <Route path="/signup" element={<StudentSignup/>}/>
             <Route path="/main" element={<PrivateRoute><TtoroChat2/></PrivateRoute>}/>
+            <Route path="/problems" element={<PrivateRoute><ProblemPage/></PrivateRoute>}/>
+            <Route path="/feedback" element={<PrivateRoute><FeedbackPage/></PrivateRoute>}/>
+            <Route path="/chat" element={<PrivateRoute><ChatPage/></PrivateRoute>}/>
+            <Route path="/tutor" element={<PrivateRoute><TutorPage/></PrivateRoute>}/>
+            <Route path="/tutorDetail/:tutorId" element={<PrivateRoute><TutorDetailPage/></PrivateRoute>}/>
+            <Route path="/progress" element={<PrivateRoute><ProgressPage/></PrivateRoute>}/>
         </Routes>
     );
 }
